b370f276f698251c4ed7af2e1bde2c932c35a740
Andrew Adel <20201700157@cis.asu.edu.eg>
2024-04-13 22:54:12 +0200

    Send dict of images as a response instead of a list

--- app.py
+++ app.py
@@ -10,10 +10,20 @@ import requests
 import io
 from io import BytesIO
 import json
+import pandas as pd
+from string import digits
+import re
+import unicodedata
+import keras_nlp
+import numpy as np
+from transformers import TFDistilBertForSequenceClassification
+from transformers import DistilBertTokenizer
+from transformers import TFPreTrainedModel
+from transformers import DistilBertTokenizer, TFDistilBertModel
 
 
 # Load the model with custom objects
-model = load_model("models/mobileNet_V3.h5")
+model = load_model("models/mobileNetV3.h5")
 
 app = Flask(__name__)
 app.config["IMAGE_UPLOADS"] = "static/Uploads/"
@@ -76,7 +86,7 @@ def getImagesList():
             # img=json.loads(images)
             images=images.split(',')
             # return jsonify({'msg': 'success', 'images-recived':images})
-            predictions=[]
+            predictions={}
             for image in images:
                 response = requests.get(image)
                 img = Image.open(io.BytesIO(response.content)).resize((224,224)).convert('RGB')
@@ -90,8 +100,8 @@ def getImagesList():
                     prediction="Violence"
                 else:
                     prediction="Non-Violence"
-                predictions.append(prediction)
-            return jsonify({'msg': 'success', 'prediction': [predictions]})
+                predictions[image]=prediction
+            return jsonify({'msg': 'success', 'prediction': predictions})
         return jsonify({"BackendError":"Images not sent"})
     return jsonify({"BackendError": "Error in request"})
 
